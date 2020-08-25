617178fe01bfe45c711f9dbb564d2ea6499b8e31
Mr-strlen <cauitexp162carl@163.com>
2020-08-25 23:05:11 +0800

    排版

--- Algorithm/04Layout.cpp
+++ Algorithm/04Layout.cpp
@@ -29,6 +29,26 @@ int main(){
 		for(int i=1,j=1;i<=n;i+=2,j++){
 			int x=n/2+1,y=x;
 			x-=j-1;y-=j-1;
+			char c=j%2==1?a:b;
+			for(int k=1;k<=i;k++){
+				outPutBuf[x+k-1][y]=c;
+				outPutBuf[x][y+k-1]=c;
+				outPutBuf[x+i-1][y+k-1]=c;
+				outPutBuf[x+k-1][y+i-1]=c;
+			} 
+		}
+		if(n!=1){
+			outPutBuf[1][1]=" ";
+			outPutBuf[n][1]=" ";
+			outPutBuf[1][n]=" ";
+			outPutBuf[n][n]=" ";
 		} 
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=n;j++){
+				printf("%c",outPutBuf[i][j]);
+			}
+			printf("\n");
+		}
 	}
+	return 0;
 } 
