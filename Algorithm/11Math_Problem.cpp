874aa68e25e10e8b1ea1e3c82070a1a5375b1836
Mr-strlen <cauitexp162carl@163.com>
2020-08-30 16:22:46 +0800

    数学问题

--- Algorithm/11Math_Problem.cpp
+++ Algorithm/11Math_Problem.cpp
@@ -38,5 +38,55 @@ int main()
 	return 0;
 }
 //进制转换
- 
+#include<cstdio>
+using namespace std;
+int main(){
+	long long a,b;
+	int m;
+	while(scanf("%d",&m)!=EOF){
+		if(m==0) break;
+		scanf("%lld%lld",&a,&b);
+		a=a+b;
+		int ans[50],size=0;
+		do{
+			ans[size++]=a%m;
+			a/=m;
+		}while(a!=0)
+		for(int i=size-1;i>=0;i--){
+			printf("%d",ans[i]); 
+		} 
+		printf("\n");
+	}
+	return 0;
+} 
+//进制互转
+#include<cstdio>
+#include<cstring>
+using namespace std;
+int main(){
+	int a,b;
+	char str[40];
+	while(scanf("%d%s%d",&a,str,&b)!=EOF){
+		int tmp=0,lenth=strlen(str),c=1;
+		for(int i=lenth-1;i>=0;i--){
+		int x;
+		if(str[i]>='0' && str[i]<='9') x=str[i]-'0';
+		else if(str[i]>='a'&&str[i]<='z') x=str[i]-'a'+10;
+		else x=str[i]-'A'+10;
+		tmp+=x*c;
+		c*=a; 
+	}
+	char ans[40],size=0;
+	do{
+		int x=tmp%b;
+		ans[size++]=(x<10)?x+'0':x-10+'A';
+		tmp/=b;
+	}while(tmp);
+	for(int i=size-1;i>=0;i--)	
+		printf("%c",ans[i]);
+	printf("\n");
+	}
+	return 0;
+} 
+//最大公约数 
  
