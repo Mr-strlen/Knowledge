0a268f9e7fa87d75183b872a4a8b2600a2b47676
Mr-strlen <cauitexp162carl@163.com>
2020-09-06 21:55:30 +0800

    数学问题

--- Algorithm/11Math_Problem.cpp
+++ Algorithm/11Math_Problem.cpp
@@ -89,4 +89,88 @@ int main(){
 	return 0;
 } 
 //最大公约数 
- 
+#include<cstdio>
+using namespace std;
+int gcd(int a,int b){
+	if(b==0)return a;
+	else return gcd(b,a%b);
+} 
+int main(){
+	int a,b;
+	while(scanf("%d%d",&a,&b)!=EOF){
+		printf("%d\n",gcd(a,b));
+	}
+	return 0;
+}
+//最小公倍数
+#include<cstdio>
+using namespace std;
+int gcd(int a,int b){
+	return b!=0?gcd(b,a%b):a;
+} 
+int main(){
+	int a,b;
+	while(scanf("%d%d",&a,&b)!=EOF){
+		printf("%d\n",a*b/gcd(a,b));
+	}
+	return 0;
+}
+//素数
+#include<cstdio>
+#include<cmath>
+using namespace std;
+bool judge(int x){
+	if(x<=1) return false;
+	int bound=(int)sqrt(x)+1;
+	for(int i=2;i<bound;i++){
+		if(x%i==0) return false;
+	}
+	return true;
+} 
+int main(){
+	int x;
+	while(scanf("%d",&x)!=EOF){
+		puts(judge(x)?"yes":"no");
+	}
+	return 0;
+}
+//素数筛法
+#include<cstdio>
+using namespace std;
+int prime[10000];
+int primeSize;
+bool mark[10001];
+void init(){
+	for(int i=1;i<=10000;i++){
+		mark[i]=false;
+	}
+	primeSize=0;
+	for(int i=2;i<=10000;i++){
+		if(mark[i]=true) continue;
+		prime[primeSize++]=i;
+		for(int j=i*i;j<=10000;j+=i){
+			mark[j]=true;
+		}
+	}
+}
+int main(){
+	init();
+	int n;
+	while(scanf("%d",&n)!=EOF){
+		bool isOutput=false;
+		for(int i=0;i<primeSize;i++){
+			if(prime[i]<n&&prime[i]%10==1){
+				if(isOutput=false){
+					isOutput=true;
+					printf("%d",prime[i]);
+				}
+				else printf("%d",prime[i]);
+			}
+		}
+		if(isOutput=false){
+			printf("-1\n");
+		}
+		else printf("\n");
+	}
+	return 0;
+}
