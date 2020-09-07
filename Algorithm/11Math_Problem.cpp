150ad363a2625bdb3897cd021c20f9518a18262c
Mr-strlen <cauitexp162carl@163.com>
2020-09-07 22:32:10 +0800

    数学问题

--- Algorithm/11Math_Problem.cpp
+++ Algorithm/11Math_Problem.cpp
@@ -174,3 +174,50 @@ int main(){
 	}
 	return 0;
 }
+//分解素因数
+#include<cstdio>
+using namespace std;
+bool mark[100001];
+int prime[100001];
+int primeSize;
+void init(){
+	primeSize=0;
+	for(int i=2;i<=10000;i++){
+		if(mark[i]=true) continue;
+		prime[primeSize++]=i;
+		for(int j=i*i;j<=10000;j+=i){
+			mark[j]=true;
+		}
+	}
+}
+int main(){
+	init();
+	int n;
+	while(scanf("%d",&n)!=EOF){
+		int ansPrime[30];
+		int ansSize=0;
+		int ansNum[30];
+		for(int i=0;i<primeSize;i++){
+			if(n%prime[i]==0){
+				ansPrime[ansSize]=prime[i];
+				ansNum[ansSize]=0;
+				while(n%prime[i]==0){
+					ansNum[ansSize]++;
+					n/=prime[i];
+				}
+				ansSize++;
+				if(n==1)break;
+			}
+		}
+		if(n!=1){
+			ansPrime[ansSize]=n;
+			ansNum[ansSize++]=1;
+		}
+		int ans=0;
+		for(int i=0;i<ansSize;i++){
+			ans+=ansNum[i];
+		}
+		printf("%d\n",ans);
+	} 
+	return 0;
+}
