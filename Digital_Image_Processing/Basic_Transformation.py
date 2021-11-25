101613f47e409a78b4bd4fd419297b92a135e346
Mr-strlen <cauitexp162carl@163.com>
2021-11-25 16:39:26 +0800

    完成了锐化，并修改了其中的错误；实现了高提升滤波；完成频率低通和高斯低通；完成高斯高通和拉普拉斯高通

--- Digital_Image_Processing/Basic_Transformation.py
+++ Digital_Image_Processing/Basic_Transformation.py
@@ -250,24 +250,24 @@ def img_medianfilter(img,k=3,padding=None):
                         new_img[y,x]=img[y,x]
         return new_img
 
-# 图像锐化 Robert交叉梯度 Sobel模板 拉普拉斯算子
-def img_sobelfilter(img,filter_kind=1,padding="replicate"):
-    new_img=np.zeros((height,width),dtype=np.int16)
-    img=img.astype(np.int64)
+## 图像锐化 Robert交叉梯度 Sobel模板 拉普拉斯算子
+def img_sobelfilter(img,filter_kind=1,padding="replicate",methods="high",A=1):
+    new_img=np.zeros((height,width),dtype=np.float64)
+    img=img.astype(np.float64)
     # 矩阵使用的是三阶，所以k=3
     k=3
     dt=int((k-1)/2)
     # 填充矩阵
     if padding=="zeros": #零填充 检测边缘基本不用0填充
-        tmp_img=np.r_[np.zeros((dt,width)),img.astype(np.int),np.zeros((dt,width))]
+        tmp_img=np.r_[np.zeros((dt,width)),img,np.zeros((dt,width))]
         tmp_img=np.hstack((np.zeros((height+k-1,dt)),tmp_img))
         tmp_img=np.hstack((tmp_img,np.zeros((height+k-1,dt))))
     elif padding=="reflection": #镜像填充
-        ttmp_img=np.r_[img[-dt:,:],img.astype(np.int),img[:dt,:]]
+        ttmp_img=np.r_[img[-dt:,:],img,img[:dt,:]]
         tmp_img=np.hstack((ttmp_img[:,-dt:],ttmp_img))
         tmp_img=np.hstack((tmp_img,ttmp_img[:,:dt]))
     elif padding=="replicate": #重复填充
-        tmp_img=np.r_[np.repeat(img[:1,:],dt,axis=0),img.astype(np.int),np.repeat(img[-1:,:],dt,axis=0)]
+        tmp_img=np.r_[np.repeat(img[:1,:],dt,axis=0),img,np.repeat(img[-1:,:],dt,axis=0)]
         tmp_img=np.hstack((np.repeat(tmp_img[:,:1],dt,axis=1),tmp_img))
         tmp_img=np.hstack((tmp_img,np.repeat(tmp_img[:,-1:],dt,axis=1)))
     
@@ -286,15 +286,71 @@ def img_sobelfilter(img,filter_kind=1,padding="replicate"):
     #第一，一阶导数通常会产生较宽的边缘；
     #第二，二阶导数对于阶跃性边缘中心产生零交叉，而对屋顶状的边缘，二阶导数取极值；
     #第三，二阶导数对细节有较强的响应，如细线和孤立噪声点。
-        
+    
+    if methods=="low":
+        for y in range(height):
+            for x in range(width):
+                temp=np.sum(filter_core*tmp_img[y:y+dt*2+1,x:x+2*dt+1])
+                new_img[y,x]=abs(temp)
+        # 映射回0-255
+        new_img=np.round(255.0*(new_img-np.min(new_img))/(np.max(new_img)-np.min(new_img)))
+        return new_img.astype(np.uint8)
+    ## 高提升滤波
+    # 基于锐化的图像增强中，可以保留原图像中的信息
+    # 所以可以将原图像和锐化后的图像相加来得到比较理想的成果
+    elif methods=="high":
+        for y in range(height):
+            for x in range(width):
+                temp=np.sum(filter_core*tmp_img[y:y+dt*2+1,x:x+2*dt+1])
+                if temp<-255:
+                    temp=-255
+                if temp>255:
+                    temp=255
+                new_img[y,x]=A*img[y,x]-temp
+        # 映射回0-255
+        new_img=255.0*(new_img-np.min(new_img))/(np.max(new_img)-np.min(new_img))
+        new_img=np.round(new_img)
+        return new_img.astype(np.uint8)
+
+## 低通滤波器 截断 高斯低通滤波器
+def img_lowpassfilter(img,methods="gauss",cut=50,sigma=1):
+    img=img.astype(np.float64)
+    fft=np.fft.fft2(img)
+    fft=np.fft.fftshift(fft) #将FFT变换之后的频谱显示范围从[0,N]变为：[-N/2,N/2-1](N为偶数)或者[-(N-1)/2,(N-1)/2](N为奇数)
     for y in range(height):
         for x in range(width):
-            temp=np.round(np.sum(filter_core*tmp_img[y:y+dt*2+1,x:x+2*dt+1]))
-            new_img[y,x]=abs(temp)
-    # 映射回0-255
-    new_img=np.round(255*(new_img-np.min(new_img))/(np.max(new_img)-np.min(new_img)))
+            if methods=="gauss":
+                fft[y,x]*=np.exp(-((y-(height-1)/2)**2+(x-(width-1)/2)**2)/2/sigma**2)
+            elif methods=="cut":
+                if ((y-(height-1)/2)**2+(x-(width-1)/2)**2)>cut**2:
+                    fft[y,x]=0
+            
+    fft=np.fft.ifftshift(fft)
+    ifft=np.fft.ifft2(fft)
     
+    ifft=np.real(ifft)
+    new_img=255*(ifft-np.min(ifft))/(np.max(ifft)-np.min(ifft))
     return new_img.astype(np.uint8)
+
+## 高通滤波器 高斯高通滤波器 拉普拉斯滤波器
+def img_highpassfilter(img,methods="gauss",sigma=1):
+    img=img.astype(np.float64)
+    fft=np.fft.fft2(img)
+    fft=np.fft.fftshift(fft) #将FFT变换之后的频谱显示范围从[0,N]变为：[-N/2,N/2-1](N为偶数)或者[-(N-1)/2,(N-1)/2](N为奇数)
+    for y in range(height):
+        for x in range(width):
+            if methods=="gauss":
+                fft[y,x]*=(1-np.exp(-((y-(height-1)/2)**2+(x-(width-1)/2)**2)/2/sigma**2))
+            elif methods=="laplace":
+                fft[y,x]*=-((y-(height-1)/2)**2+(x-(width-1)/2)**2)
+            
+    fft=np.fft.ifftshift(fft)
+    ifft=np.fft.ifft2(fft)
+    
+    ifft=np.real(ifft)
+    new_img=255*(ifft-np.min(ifft))/(np.max(ifft)-np.min(ifft))
+    return new_img.astype(np.uint8)
+    
 
 plt.close("all")
 # img_origin=cv2.imread('./test/Lena.bmp')
@@ -304,6 +360,7 @@ img_origin=cv2.imread('./test/wife_middle.jfif')
 height,width=img_origin.shape[:2]
 img_gray=img_rgb2gray(img_origin)
 cv2.imshow("test",img_gray)
+cv2.imwrite("wife_gray.jpg",img_gray)
 # img_grayhist(img_gray)
 # cv2.imshow("reverse",img_graytransform_reverse(img_gray))
 # cv2.imshow("graytransform",img_graytransform(img_gray,gama=0.7))
@@ -325,10 +382,29 @@ cv2.imshow("test",img_gray)
 # img_new=img_medianfilter(img_noise)
 # cv2.imshow("test2",img_new)
 
-# cv2.imshow("test1",img_sobelfilter(img_gray,filter_kind=1))
-# cv2.imshow("test2",img_sobelfilter(img_gray,filter_kind=2))
-# cv2.imshow("test3",img_sobelfilter(img_gray,filter_kind=3))
-# cv2.imshow("test4",img_sobelfilter(img_gray,filter_kind=4))
-# cv2.imshow("test5",img_sobelfilter(img_gray,filter_kind=5))
+# cv2.imshow("test1",img_sobelfilter(img_gray,filter_kind=3,method="high",A=1))
+# cv2.imshow("test2",img_sobelfilter(img_gray,filter_kind=3,method="high",A=2))
+# cv2.imshow("test3",img_sobelfilter(img_gray,filter_kind=4,method="high",A=5))
+# cv2.imshow("test2",img_lowpassfilter(img_gray,methods="cut",cut=50))
+# cv2.imshow("test2",img_highpassfilter(img_gray,methods="gauss",sigma=20))
+# cv2.imshow("test2",img_highpassfilter(img_gray,methods="laplace"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
